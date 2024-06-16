5c3fc7ef4fbaa88907bf5c431338409e18393446
yamaserif <62947068+yamaserif@users.noreply.github.com>
2024-06-17 03:27:05 +0900

    音声ファイル保存周り修正、フォーマット指定機能実装

--- src/EasySpeechCorpusCreator/EasySpeechCorpusCreator/Consts/SettingsConst.cs
+++ src/EasySpeechCorpusCreator/EasySpeechCorpusCreator/Consts/SettingsConst.cs
@@ -11,6 +11,7 @@
         public const string EXPLAIN_EXTERNAL_SOFTWARE = "● 外部ソフト連携：\n音声ファイルの外部ソフト連携用コマンドを設定します。\n%VOICE_FILE%：コーパス音声データファイルのパス\n%S%：文字列としてのスペース";
         public const string EXPLAIN_DEVICE = "● 録音デバイス：\n録音に使用するデバイスを選択します。";
         public const string EXPLAIN_SAMPLING_RATE = "● サンプリングレート(Hz)：\n録音時のサンプリングレートを設定します";
+        public const string EXPLAIN_SAVE_VOICE_FORMAT = "● 音声ファイル保存時のフォーマット：\n音声ファイルを保存する際のファイル名を設定します。\n%No%：No.\n%Name%：名前\n%Sentence%：文章\n%Kana%：カナ";
 
         // デフォルト値
         public const string DEFAULT_PROJECT_PASS = @".\..\projects\";
@@ -18,6 +19,7 @@
         public const string DEFAULT_EXTERNAL_SOFTWARE = "explorer.exe \"%VOICE_FILE%\"";
         public const string DEFAULT_DEVICE = "";
         public const int DEFAULT_SAMPLING_RATE = 44100;
+        public const string DEFAULT_SAVE_VOICE_FORMAT = "%No%_%Name%";
 
         public const string EXTERNAL_SOFTWARE_PATH = "%VOICE_FILE%";
         public const string EXTERNAL_SOFTWARE_SPACE = "%S%";
